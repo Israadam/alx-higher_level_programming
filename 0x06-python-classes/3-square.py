e8a4085657db26112bb15cd7943279ea970d2cb6
israadam <israaosama796@gmail.com>
2023-12-19 07:13:34 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
 """Square module."""
 
-
 class Square:
-    """Defines a square."""
+"""Defines a square."""
 
-    def __init__(self, size=0):
-        """Constructor.
+def __init__(self, size=0):
+"""Constructor.
 
-        Args:
-        size: Length of a side of the square.
+Args:
+size: Length of a side of the square.
         
-        Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is less than 0.
-        """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+Raises:
+TypeError: If size is not an integer.
+ValueError: If size is less than 0.
+"""
+if not isinstance(size, int):
+raise TypeError('size must be an integer')
+if size < 0:
+raise ValueError('size must be >= 0')
+self.__size = size
 
-        def area(self_:
-                """Area of this square.
+def area(self_:
+"""Area of this square.
 
-                Returns:
-                The size squared.
-                """
-                return self.__size ** 2
+Returns:
+The size squared.
+"""
+return self.__size ** 2
