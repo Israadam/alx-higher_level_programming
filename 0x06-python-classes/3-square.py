90e46b15d9242f6ff2ac80b0de0babeb5ffc865e
israadam <israaosama796@gmail.com>
2023-12-19 07:06:23 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,14 +4,18 @@
 
 class Square:
     """Defines a square."""
+
     def __init__(self, size=0):
         """Constructor.
+
         Args:
         size: Length of a side of the square.
+        
         Raises:
         TypeError: If size is not an integer
         ValueError: If size is less than 0
         """
+
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
@@ -24,4 +28,4 @@ class Square:
                 Returns:
                 The size squared.
                 """
-                return self.__size** 2
+                return self.__size ** 2
