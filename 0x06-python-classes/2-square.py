d13454dc202325fe4e3ddb30c47b8d28f3802e6a
israadam <israaosama796@gmail.com>
2023-12-19 07:03:29 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,14 +4,18 @@
 
 class Square:
     """Defines a square."""
+
     def __init__(self, size=0):
         """Constructor.
+
         Args:
         size: Length of a side of the square.
+
         Raises:
         TypeError: If size is not an integer
         ValueError: If size is less than 0
         """
+
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
