171b11da6792fefaa89989378cf18409a3777241
israadam <israaosama796@gmail.com>
2023-12-19 06:56:30 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,10 +2,10 @@
 """Square module."""
 
 class square:
-    """Defines a square."""
-    def __init__(self, size):
-        """constructor.
-        Args:
-        size: length of a side of the square.
-        """
-        self.__size = size
+"""Defines a square."""
+def __init__(self, size):
+"""Constructor.
+Args:
+size: length of a side of the square.
+"""
+self.__size = size
