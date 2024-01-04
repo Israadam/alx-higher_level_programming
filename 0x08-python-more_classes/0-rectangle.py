ce08564a7c9b67562ecce4f14829bbbd4c60bd97
israadam <israaosama796@gmail.com>
2024-01-04 07:00:42 -0800

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""0-rectangle, built for Holberton project 0x08 task 0.
+"""
+Defining an empty class Rectangle
 """
 
 
 class Rectangle:
-"""Empty class per task instructions, will be built upon in later tasks.
+"""Empty representation of a Rectangle
 """
 pass
